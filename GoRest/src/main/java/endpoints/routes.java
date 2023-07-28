065e009b9058a1a8d018f1bd4b201c6c5cb75030
venkat <venkatgn.gutta@gmail.com>
2023-07-28 07:45:10 +0530

    updated routes file

---
+++ GoRest/src/main/java/endpoints/routes.java
@@ -0,0 +1,12 @@
+package endpoints;
+
+public class routes 
+{
+   public static String baseurl="https://gorest.co.in/";   
+   public static String posturl=baseurl+"public/v2/users";
+   public static String geturl=baseurl+"public/v2/users/{userid}";
+   public static String updateurl=baseurl+"public/v2/users/{userid}";
+   public static String deleteurl=baseurl+"public/v2/users/{userid}";  
+   
+   
+}
